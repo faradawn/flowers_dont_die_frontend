35fb595bacaa6379ff899586d0f663ba4375fa2b
faradawn <faradawny@gmail.com>
2024-07-28 22:53:19 -0500

    add keyboard avoiding view for login screen

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, Image, ImageBackground, Dimensions, TextInput,
-    Text, TouchableOpacity, Keyboard, TouchableWithoutFeedback
+    Text, TouchableOpacity, Keyboard, TouchableWithoutFeedback, Platform, KeyboardAvoidingView
 } from 'react-native';
 
 import { globalStyles } from '../globalStyles/globalStyles';
@@ -63,158 +63,161 @@ export default function Login({ navigation }){
                 ...globalStyles.container
             }}
         >
-            {/* Flower Icon Header */}
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View
-                    style={ {
-                        height: height * 0.4,
-                        width: width,
-                        alignItems: 'center',
-                        justifyContent: 'flex-end',
-                    } }
-                >
-                    <Image
-                        source={require('../../assets/images/FlowerIcon.jpg')}
-                        style={ { 
-                            marginBottom: 0.05 * height,
-                            height: 150,
-                            width: 150,
-                        } }
-                    />
-                </View>
-            </TouchableWithoutFeedback>
-
-            {/* Text Input Component for Username and Password */}
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View
-                    style={{
-                        height: height * 0.3,
-                        width: width * 0.8,
-                        alignItems: 'flex-start',
-                        justifyContent: 'flex-end',
-                    }}
-                >
+                <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{alignItems: "center", justifyContent: "center"}}>
                     
-                    {/* Username input field */}
-                    <Text style={globalStyles.inputKey}> Username </Text>
-                    <TextInput 
-                        style={[
-                            { 
-                                height: 0.06 * height, 
-                                width: 0.8 * width,
-                                paddingHorizontal: 20, 
-                            }, 
-                            globalStyles.textInput
-                        ]}
-                        placeholder='John Smith'
-                        placeholderTextColor='rgba(255, 255, 255, 0.5)'
-                        onChangeText={(val) => setUsername(val)}
-                        value={username}
-                        
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                    />
+                    
+                    {/* 1. Flower icon */}
+                    <View
+                        style={ {
+                            height: height * 0.4,
+                            width: width,
+                            alignItems: 'center',
+                            justifyContent: 'flex-end',
+                        } }
+                    >
+                        <Image
+                            source={require('../../assets/images/FlowerIcon.jpg')}
+                            style={ { 
+                                marginBottom: 0.05 * height,
+                                height: 150,
+                                width: 150,
+                            } }
+                        />
+                    </View>
 
-                    {/* Password input field */}
-                    <Text style={globalStyles.inputKey}> Password </Text>
-                    <TextInput 
-                        style={[
-                            { 
-                                height: 0.06 * height, 
-                                width: 0.8 * width,
-                                paddingHorizontal: 20, 
-                            }, 
-                            globalStyles.textInput
-                        ]}
-                        placeholder="John Smith's Password"
-                        placeholderTextColor='rgba(255, 255, 255, 0.5)'
-                        onChangeText={(val) => setPassword(val)}
-                        value={password}
-                        
-                        textContentType="oneTimeCode"
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        
-                        secureTextEntry={true}
-                    />
 
-                    {/* Error message field */}
-                    { infoCorrect ?
-                        (
-                            <View style={{ height: 23 }}>
-                            </View>
-                        ) 
-                        : 
-                        (
-                            <Text
-                                style={{
-                                    fontFamily: 'Baloo2-Bold',
-                                    color: '#FFD912',
-                                    alignSelf: 'center',
-                                }}
-                            >
-                                Invalid username or wrong password.
-                            </Text>
-                        ) 
-                    }
-                </View>
-            </TouchableWithoutFeedback>
-
-            {/* Login Button */}
-            <View
-                style={{ 
-                    height: 0.1 * height,
-                    width: 0.8 * width,
-
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                <TouchableOpacity
-                    style={ [
-                        { 
-                            backgroundColor: '#F8C660',
-                            height: 0.06 * height,
-                            width: 0.8 * width,
-                        }, 
-                        globalStyles.button
-                    ] }
-                    onPress={() => loginAttempt()}
-                >
-                    <Text style={globalStyles.buttonText}>Login</Text>
-                </TouchableOpacity>
-            </View>
-
-            {/* Sign Up Message */}
-            <View 
-                style={{ 
-
-                    height: 0.2 * height, 
-                    flexDirection: 'row' 
-                }}
-            >
-                <Text
-                    style= {{ 
-                        fontFamily: 'Baloo2-Bold',
-                        fontSize: 16,
-                        color: 'white',
-                    }}
-                >
-                    Don't have an account? 
-                </Text>
-                <TouchableOpacity
-                    onPress={() => navigation.navigate('SignUp')}
-                >
-                    <Text
-                        style= {{
-                            fontFamily: 'Baloo2-Bold',
-                            fontSize: 16,
-                            color: '#FFD912',
+                    {/* 2. Forms */}
+                    <View
+                        style={{
+                            height: height * 0.3,
+                            width: width * 0.8,
+                            alignItems: 'flex-start',
+                            justifyContent: 'flex-end',
                         }}
-                    > Sign up.</Text>
-                </TouchableOpacity>
-            </View>
+                    >
+                        
+                        {/* Username input field */}
+                        <Text style={globalStyles.inputKey}> Username </Text>
+                        <TextInput 
+                            style={[
+                                { 
+                                    height: 0.06 * height, 
+                                    width: 0.8 * width,
+                                    paddingHorizontal: 20, 
+                                }, 
+                                globalStyles.textInput
+                            ]}
+                            placeholder='John Smith'
+                            placeholderTextColor='rgba(255, 255, 255, 0.5)'
+                            onChangeText={(val) => setUsername(val)}
+                            value={username}
+                            
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                        />
 
+                        {/* Password input field */}
+                        <Text style={globalStyles.inputKey}> Password </Text>
+                        <TextInput 
+                            style={[
+                                { 
+                                    height: 0.06 * height, 
+                                    width: 0.8 * width,
+                                    paddingHorizontal: 20, 
+                                }, 
+                                globalStyles.textInput
+                            ]}
+                            placeholder="John Smith's Password"
+                            placeholderTextColor='rgba(255, 255, 255, 0.5)'
+                            onChangeText={(val) => setPassword(val)}
+                            value={password}
+                            
+                            textContentType="oneTimeCode"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            
+                            secureTextEntry={true}
+                        />
+
+                        {/* Error message field */}
+                        { infoCorrect ?
+                            (
+                                <View style={{ height: 23 }}>
+                                </View>
+                            ) 
+                            : 
+                            (
+                                <Text
+                                    style={{
+                                        fontFamily: 'Baloo2-Bold',
+                                        color: '#FFD912',
+                                        alignSelf: 'center',
+                                    }}
+                                >
+                                    Invalid username or wrong password.
+                                </Text>
+                            ) 
+                        }
+                    </View>
+
+                    {/* 3. Login Button */}
+                    <View
+                        style={{ 
+                            height: 0.1 * height,
+                            width: 0.8 * width,
+
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <TouchableOpacity
+                            style={ [
+                                { 
+                                    backgroundColor: '#F8C660',
+                                    height: 0.06 * height,
+                                    width: 0.8 * width,
+                                }, 
+                                globalStyles.button
+                            ] }
+                            onPress={() => loginAttempt()}
+                        >
+                            <Text style={globalStyles.buttonText}>Login</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    {/* Sign Up Message */}
+                    <View 
+                        style={{ 
+
+                            height: 0.2 * height, 
+                            flexDirection: 'row' 
+                        }}
+                    >
+                        <Text
+                            style= {{ 
+                                fontFamily: 'Baloo2-Bold',
+                                fontSize: 16,
+                                color: 'white',
+                            }}
+                        >
+                            Don't have an account? 
+                        </Text>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('SignUp')}
+                        >
+                            <Text
+                                style= {{
+                                    fontFamily: 'Baloo2-Bold',
+                                    fontSize: 16,
+                                    color: '#FFD912',
+                                }}
+                            > Sign up.</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </KeyboardAvoidingView>            
+            </TouchableWithoutFeedback>
         </ImageBackground>
     )
 }
