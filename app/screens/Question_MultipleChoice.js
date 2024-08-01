8ffb654e70db3d847d4491033068e1b121137292
KongBai Mark <markzhang0727@gmail.com>
2024-08-01 10:11:20 +0800

    Color button (#7)
    * Add the submission feedback feature. Set the correct choice in green and set the incorrect choices in red.
    
    * Add submitting feedback.
    
    ---------
    
    Co-authored-by: zqyxl <zqyxl@zqyxldeMacBook-Pro.local>
    Co-authored-by: zqyxl <zqyxl@192.168.1.7>

--- app/screens/Question_MultipleChoice.js
+++ app/screens/Question_MultipleChoice.js
@@ -290,7 +290,6 @@ export default function Question_Combined({ navigation, route }) {
         </View>
     );
 
-
     const ModalComponent = () => (
         <Modal
             visible={modalOpen}
@@ -457,9 +456,6 @@ export default function Question_Combined({ navigation, route }) {
         </View>
     );
 
-
-
-
     const NextButtonComponent = () => (
         <View style={{ 
             width: width,
@@ -509,7 +505,7 @@ export default function Question_Combined({ navigation, route }) {
                   <QuestionComponent />
 
                   <View style={{height: 20}} />
-                
+    
 
                   <SwitchButton
                     FirstText="Voice"
@@ -617,6 +613,8 @@ export default function Question_Combined({ navigation, route }) {
                                 width={width * 0.8}
                                 height={height * 0.35}
                                 isSelected={currentPressed === optionLetter}
+                                isCardSubmitted={submitted}
+                                isCardCorrectAnswer={optionLetter == data.answer}
                             />
                             );
                         })}
