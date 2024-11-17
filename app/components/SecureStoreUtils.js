545e46edc15ee8f5f3d4cb5ed6c1de3b78ff6875
faradawn <faradawny@gmail.com>
2024-11-17 17:07:44 -0600

    remove dist folder and add secure store back

--- app/components/SecureStoreUtils.js
+++ app/components/SecureStoreUtils.js
@@ -1,33 +1,15 @@
-// import * as SecureStore from 'expo-secure-store';
-
-// This mock version is provided so that we have a functioning deployed version
+import * as SecureStore from 'expo-secure-store';
 
 export async function saveLoginInfo(uid, username, password) {
-  console.log('Mock saveLoginInfo called');
-  // No operation performed
+  const loginInfo = JSON.stringify({ uid, username, password });
+  await SecureStore.setItemAsync('login-info', loginInfo);
 }
 
 export async function getLoginInfo() {
-  console.log('Mock getLoginInfo called');
-  // Return null to indicate no login info
-  return null;
+  const loginInfo = await SecureStore.getItemAsync('login-info');
+  return loginInfo ? JSON.parse(loginInfo) : null;
 }
 
 export async function deleteLoginInfo() {
-  console.log('Mock deleteLoginInfo called');
-  // No operation performed
+  await SecureStore.deleteItemAsync('login-info');
 }
-
-// export async function saveLoginInfo(uid, username, password) {
-//   const loginInfo = JSON.stringify({ uid, username, password });
-//   await SecureStore.setItemAsync('login-info', loginInfo);
-// }
-
-// export async function getLoginInfo() {
-//   const loginInfo = await SecureStore.getItemAsync('login-info');
-//   return loginInfo ? JSON.parse(loginInfo) : null;
-// }
-
-// export async function deleteLoginInfo() {
-//   await SecureStore.deleteItemAsync('login-info');
-// }
